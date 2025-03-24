8802d2f20abda53f70ae923d12975f2fe91faba4
outlandergithub <148538105+outlandergithub@users.noreply.github.com>
2025-03-24 16:53:48 +0200

    Updated fixture to run headless browsers

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture
 def driver():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     browser = webdriver.Firefox(options=options)
     browser.maximize_window()
     browser.implicitly_wait(5)
